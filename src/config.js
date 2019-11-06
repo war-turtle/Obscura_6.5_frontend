12b4883cc5e53179679ee64afa66bd5eb779bee5
war-turtle <kartikyadav71199@gmail.com>
2019-11-06 21:12:16 +0530

    frontend added into envoy proxy

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ const config = {
   prod: {
     ClientId:
       "770672030537-fs29b8dfck7ctm2038e26caq7qcm760i.apps.googleusercontent.com",
-    UsersBackend: "http://obscuranitkkr.co.in:8080/users",
-    LevelBackend: "http://obscuranitkkr.co.in:8080/players"
+    UsersBackend: "http://localhost:8080/users",
+    LevelBackend: "http://localhost:8080/players"
   }
 };
 
