31215d056e4a65276efca951e2f582ac48735f53
war-turtle <kartikyadav71199@gmail.com>
2019-11-06 22:37:12 +0530

    backend url swifted over https

--- src/config.js
+++ src/config.js
@@ -1,18 +1,14 @@
 const config = {
   dev: {
-    ClientId:
-      "770672030537-fs29b8dfck7ctm2038e26caq7qcm760i.apps.googleusercontent.com",
-    UsersBackend: "http://localhost:8080/users",
-    LevelBackend: "http://localhost:8080/players"
+    ClientId: '770672030537-fs29b8dfck7ctm2038e26caq7qcm760i.apps.googleusercontent.com',
+    UsersBackend: 'https://obscuranitkkr.co.in:8080/users',
+    LevelBackend: 'https://obscuranitkkr.co.in:8080/players',
   },
   prod: {
-    ClientId:
-      "770672030537-fs29b8dfck7ctm2038e26caq7qcm760i.apps.googleusercontent.com",
-    UsersBackend: "http://localhost:8080/users",
-    LevelBackend: "http://localhost:8080/players"
-  }
+    ClientId: '770672030537-fs29b8dfck7ctm2038e26caq7qcm760i.apps.googleusercontent.com',
+    UsersBackend: 'https://obscuranitkkr.co:8080/users',
+    LevelBackend: 'https://localhost:8080/players',
+  },
 };
 
-export default process.env.REACT_APP_STAGE === "prod"
-  ? config.prod
-  : config.dev;
+export default process.env.REACT_APP_STAGE === 'prod' ? config.prod : config.dev;
